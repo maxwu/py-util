cc35769d006982533c5493f96db9445693db6f33
maxwu <maxwunj@gmail.com>
2017-01-15 23:38:23 +1300

    Add AWS SES NodeJS sample

--- aws-tools/lambda/s3_triger_email_nodejs.js
+++ aws-tools/lambda/s3_triger_email_nodejs.js
@@ -1,34 +1,23 @@
-"use strict";
+'use strict';
 
-console.log('Loading function');
-
+console.log('Loading lambda on_post_put');
 const aws = require('aws-sdk');
+const s3 = new aws.S3({apiVersion: '2006-03-01'});
+var ses = new aws.SES();
 
-const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
-
-exports.handler = (event, context, callback) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
-
+exports.handler = (event, context, callback) =>
+{
+    // console.log('Received event:', JSON.stringify(event, null, 2));
     // Get the object from the event and show its content type
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-    const params = {
+    const s3_params = {
         Bucket: bucket,
         Key: key,
     };
-    
-    // Added Logics, --Max, 2016-12-19
-    let max = 100000;
-    let min = 0;
-    let rnum = Math.floor(Math.random()*max + min);
-    function prefix_integer(num, length) {
-        return (Array(length).join('0') + num).slice(-length);
-    }
-    let snum = prefix_integer(rnum, 5) 
-    console.log ("generated random number is " + snum);
-    
-    s3.getObject(params, (err, data) => {
+
+    s3.getObject(s3_params, (err, data) => {
         if (err) {
             console.log(err);
             const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
@@ -36,10 +25,47 @@ exports.handler = (event, context, callback) => {
             callback(message);
         } else {
             console.log('CONTENT TYPE:', data.ContentType);
-            callback(null, data.ContentType);
+
+    //begin SES block, --Max
+    var msg_params = {
+        Destination: {
+            /* required */
+            ToAddresses: [
+                'maxwunj@gmail.com',
+                /* more items */
+            ]
+        },
+        Message: {
+            /* required */
+            Body: {
+                /* required */
+                Text: {
+                    Data: `Notification from AWS SES. You have a new uploaded MD file ${key} in ${bucket}`, /* required */
+                    Charset: 'UTF-8'
+                }
+            },
+            Subject: {
+                /* required */
+                Data: `New post added -- ${key}` , /* required */
+                Charset: 'UTF-8'
+            }
+        },
+        Source: 'Contact to Max Wu', /* required */
+        ReplyToAddresses: [
+            'maxwunj@msn.com',
+            /* more items */
+        ]
+    };  /* msg_params */
+    ses.sendEmail(msg_params,
+        function (err, data) {
+            if (err)
+                console.log(err, err.stack); // an error occurred
+            else
+                console.log(data);           // successful response
         }
-    });
-    
+    );
 
+    callback(null, data.ContentType);
+}  /* if-else */
+});
 };
-
